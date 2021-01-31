6d97561a12a18b9c30180e8829c9b3c6eade8b1e
Shavrin Maksim <nutmegraw@gmail.com>
2021-01-31 21:40:25 +0500

    Models added

--- DjangoGramm/djangogramm/models.py
+++ DjangoGramm/djangogramm/models.py
@@ -1,20 +1,54 @@
-from django.db.models import Model
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.db.models import (CharField, DateTimeField, ForeignKey, Model,
+                              TextField)
 
 # Create your models here.
 
 
-class Account(AbstractUser):
-    pass
+class Account(User):
+    reg_confirmed_date = DateTimeField()
+
+    def confirm_registration():
+        pass
+
+    def post():
+        pass
 
 
 class Post(Model):
-    pass
+    text = TextField()
+    posted_time = DateTimeField()
+    edited_time = DateTimeField()
+    account = ForeignKey(Account)
+
+    def like_unlike():
+        pass
+
+    def comment():
+        pass
+
+
+class Picture(Model):
+    picture_itself = CharField()
+    preview = CharField()
+    post = ForeignKey(Post)
 
 
 class Comment(Model):
-    pass
+    text = TextField()
+    images_refs = CharField()
+    posted_time = DateTimeField()
+    edited_time = DateTimeField()
+    post = ForeignKey(Post)
+    author = ForeignKey(Account)
+
+    def like_unlike():
+        pass
 
 
 class Like(Model):
-    pass
+    picture = ForeignKey(Picture)
+    post = ForeignKey(Post)
+    comment = ForeignKey(Comment)
+    author = ForeignKey(Account)
+    time = DateTimeField()
