4f06701aa0f63c5fe48ac4c3dc1b7dfbe1cd97bf
Shavrin Maksim <nutmegraw@gmail.com>
2021-02-06 10:16:24 +0500

    views sketches added

--- DjangoGramm/djangogramm/views.py
+++ DjangoGramm/djangogramm/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def show_login_page(request):
+    return render(request, 'login.html')
+
+
+def login():
+    pass
+
+
+def show_registration_page(request):
+    return render(request, 'registration.html')
+
+
+def regisrate():
+    pass
+
+
+def confirm_registration():
+    pass
+
+
+def show_wall(request):
+    return render(request, 'wall.html')
