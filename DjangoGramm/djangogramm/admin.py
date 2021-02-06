1019739341faeb694964a6ae0ebda21516f4e737
Shavrin Maksim <nutmegraw@gmail.com>
2021-02-06 10:09:02 +0500

    Modeks registered for admin

--- DjangoGramm/djangogramm/admin.py
+++ DjangoGramm/djangogramm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Account, Comment, Like, Picture, Post
+
 # Register your models here.
+
+
+admin.register(Account, Comment, Like, Picture, Post)
