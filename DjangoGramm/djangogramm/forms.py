fa74f5752f296466c35eacde39e61b364a0cdeed
Shavrin Maksim <nutmegraw@gmail.com>
2021-02-10 01:09:23 +0500

    Standard forms are added

---
+++ DjangoGramm/djangogramm/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
+                                       UserCreationForm)
+
+from .models import Account
+
+
+class LoginForm(AuthenticationForm):
+    pass
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = Account
+        fields = ('email', 'first_name', 'last_name',)
+
+
+class AccountChangeForm(UserChangeForm):
+    class Meta:
+        model = Account
+        fields = ('about_yourself',)
