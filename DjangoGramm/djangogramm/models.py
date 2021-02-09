5aef89be5c80766ba4f6a1ca64794e4258cb6c1b
Shavrin Maksim <nutmegraw@gmail.com>
2021-02-10 00:59:02 +0500

    Account model is changed and ForeighnKey fields related_name attribs defined

--- DjangoGramm/djangogramm/models.py
+++ DjangoGramm/djangogramm/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
-from django.db.models import (CASCADE, DO_NOTHING, DateTimeField, ForeignKey,
-                              ImageField, Model, TextField)
+from django.contrib.auth.models import AbstractUser
+from django.db.models import (CASCADE, SET_NULL, DateTimeField, EmailField,
+                              ForeignKey, ImageField, Model, OneToOneField,
+                              TextField)
 
 PICTURE_PATH = 'photos/%Y/%m/%d/'
 
@@ -8,11 +9,13 @@ PICTURE_PATH = 'photos/%Y/%m/%d/'
 # Create your models here.
 
 
-class Account(User):
+class Account(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     username = None
+    email = EmailField(unique=True)
     reg_confirmed_date = DateTimeField(auto_now_add=True)
     about_yourself = TextField(blank=True)
-    avatar = ImageField(upload_to=PICTURE_PATH, blank=True)
 
     def __str__(self):
         return f'{self.get_full_name()}, {self.EMAIL_FIELD}'
@@ -28,7 +31,7 @@ class Post(Model):
     text = TextField(blank=True)
     posted_time = DateTimeField(auto_now_add=True)
     edited_time = DateTimeField(auto_now=True, blank=True)
-    author = ForeignKey(Account, on_delete=DO_NOTHING)
+    author = ForeignKey(Account, on_delete=CASCADE, related_name='posts')
 
     def __str__(self):
         txt = 'No text in the Post'
@@ -44,11 +47,16 @@ class Post(Model):
 
 
 class Picture(Model):
+    uploader = OneToOneField(
+        Account, on_delete=SET_NULL, null=True, related_name='uploaded_pictures')
     loading_time = DateTimeField(auto_now_add=True)
     picture_itself = ImageField(upload_to=PICTURE_PATH)
     description = TextField(blank=True)
     picture_preview = ImageField(upload_to=PICTURE_PATH)
-    post = ForeignKey(Post, on_delete=DO_NOTHING)
+    post = ForeignKey(
+        Post, on_delete=SET_NULL, blank=True, null=True, related_name='pictures')
+    avatar_of = OneToOneField(
+        Account, on_delete=SET_NULL, blank=True, null=True, related_name='avatar')
 
     def like_unlike():
         pass
@@ -58,8 +66,8 @@ class Comment(Model):
     text = TextField()
     posted_time = DateTimeField(auto_now_add=True)
     edited_time = DateTimeField(auto_now=True, blank=True)
-    post = ForeignKey(Post, on_delete=CASCADE)
-    author = ForeignKey(Account, on_delete=DO_NOTHING)
+    post = ForeignKey(Post, on_delete=CASCADE, related_name='comments')
+    author = ForeignKey(Account, on_delete=CASCADE, related_name='comments')
 
     def __str__(self):
         return f'{self.author}, {self.text}, {self.posted_time}'
@@ -69,10 +77,10 @@ class Comment(Model):
 
 
 class Like(Model):
-    picture = ForeignKey(Picture, on_delete=CASCADE, blank=True)
-    post = ForeignKey(Post, on_delete=CASCADE, blank=True)
-    comment = ForeignKey(Comment, on_delete=CASCADE, blank=True)
-    author = ForeignKey(Account, on_delete=DO_NOTHING)
+    picture = ForeignKey(Picture, on_delete=CASCADE, blank=True, related_name='likes')
+    post = ForeignKey(Post, on_delete=CASCADE, blank=True, related_name='likes')
+    comment = ForeignKey(Comment, on_delete=CASCADE, blank=True, related_name='likes')
+    author = ForeignKey(Account, on_delete=CASCADE, related_name='likes')
     time = DateTimeField(auto_now_add=True)
 
     def __str__(self):
