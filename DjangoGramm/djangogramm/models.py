97af9a13d0b934fe1a28944d1faf50a93af2f58d
Shavrin Maksim <nutmegraw@gmail.com>
2021-03-10 19:33:51 +0500

    slug fields are added and Models save functon took-out

--- DjangoGramm/djangogramm/models.py
+++ DjangoGramm/djangogramm/models.py
@@ -1,18 +1,30 @@
-from random import randint
+from random import choices, randint
+from string import ascii_letters, digits
 
 from django.contrib.auth.models import AbstractUser
 from django.db.models import (CASCADE, SET_NULL, DateTimeField, EmailField,
-                              ForeignKey, ImageField, IntegerField, Model,
-                              OneToOneField, TextField)
+                              ForeignKey, ImageField, Model, OneToOneField,
+                              SlugField, TextField)
 
 PICTURE_PATH = 'photos/%Y/%m/%d/'
 
+
+def save_with_random_slug(obj, slug_lenght: int, letters=True, *args, **kwargs):
+    if not obj.slug:
+        is_unique = False
+        while not is_unique:
+            slug = str(randint(0, int('9'*slug_lenght)))
+            if letters:
+                slug = ''.join(choices(digits + ascii_letters, k=slug_lenght))
+            is_unique = (obj.__class__.objects.filter(slug=slug).count() == 0)
+        obj.slug = slug
+    super(obj.__class__, obj).save(*args, **kwargs)
 
 # Create your models here.
 
 
 class Account(AbstractUser):
-    id = IntegerField(primary_key=True, unique=True)
+    slug = SlugField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     username = None
@@ -24,18 +36,12 @@ class Account(AbstractUser):
     def __str__(self):
         return f'< Account : {self.get_full_name()} >'
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            is_unique = False
-            while not is_unique:
-                id = randint(0, int('9'*10))  # 10 digits
-                is_unique = (Account.objects.filter(id=id).count() == 0)
-            self.id = id
-        super(Account, self).save(*args, **kwargs)
+    def save(self):
+        return save_with_random_slug(self, 10, letters=False)
 
 
 class Post(Model):
-    id = IntegerField(primary_key=True, unique=True)
+    slug = SlugField(unique=True)
     text = TextField(max_length=2200, blank=True)
     posted_time = DateTimeField(auto_now_add=True, editable=False)
     edited_time = DateTimeField(blank=True, null=True)
@@ -50,18 +56,12 @@ class Post(Model):
             txt = self.text[:10]
         return f'< Post : {txt}... >'
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            is_unique = False
-            while not is_unique:
-                id = randint(0, int('9'*10))  # 10 digits
-                is_unique = (Post.objects.filter(id=id).count() == 0)
-            self.id = id
-        super(Post, self).save(*args, **kwargs)
+    def save(self):
+        return save_with_random_slug(self, 10)
 
 
 class Picture(Model):
-    id = IntegerField(primary_key=True, unique=True)
+    slug = SlugField(unique=True)
     uploader = ForeignKey(
         Account, on_delete=SET_NULL, null=True, related_name='uploaded_pictures')
     loading_time = DateTimeField(auto_now_add=True)
@@ -78,18 +78,12 @@ class Picture(Model):
     def __str__(self):
         return f'< Picture : {self.id} >'
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            is_unique = False
-            while not is_unique:
-                id = randint(0, int('9'*10))  # 10 digits
-                is_unique = (Picture.objects.filter(id=id).count() == 0)
-            self.id = id
-        super(Picture, self).save(*args, **kwargs)
+    def save(self):
+        return save_with_random_slug(self, 10)
 
 
 class Comment(Model):
-    id = IntegerField(primary_key=True, unique=True)
+    slug = SlugField(unique=True)
     text = TextField(max_length=1000)
     posted_time = DateTimeField(auto_now_add=True)
     edited_time = DateTimeField(auto_now=True, blank=True, null=True)
@@ -103,18 +97,12 @@ class Comment(Model):
         txt = self.text[:10]
         return f'< Comment : {self.author.get_full_name} | {txt}... >'
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            is_unique = False
-            while not is_unique:
-                id = randint(0, int('9'*10))  # 10 digits
-                is_unique = (Comment.objects.filter(id=id).count() == 0)
-            self.id = id
-        super(Comment, self).save(*args, **kwargs)
+    def save(self):
+        return save_with_random_slug(self, 10)
 
 
 class Like(Model):
-    id = IntegerField(primary_key=True, unique=True)
+    slug = SlugField(unique=True)
     picture = ForeignKey(Picture, on_delete=CASCADE, blank=True, related_name='likes')
     post = ForeignKey(Post, on_delete=CASCADE, blank=True, related_name='likes')
     comment = ForeignKey(Comment, on_delete=CASCADE, blank=True, related_name='likes')
@@ -127,11 +115,5 @@ class Like(Model):
     def __str__(self):
         return f'< Like : {self.author.get_full_name()} | {self.time}>'
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            is_unique = False
-            while not is_unique:
-                id = randint(0, int('9'*10))  # 10 digits
-                is_unique = (Like.objects.filter(id=id).count() == 0)
-            self.id = id
-        super(Like, self).save(*args, **kwargs)
+    def save(self):
+        return save_with_random_slug(self, 10)
