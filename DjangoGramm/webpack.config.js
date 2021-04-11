c00f89a63784c720eb116c91b7d6d83f104ba678
Shavrin Maksim <nutmegraw@gmail.com>
2021-04-12 00:55:45 +0500

    new plugins added

--- DjangoGramm/webpack.config.js
+++ DjangoGramm/webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path')
 const HTMLWebpuckPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
 
 module.exports = {
     context: path.resolve(__dirname, "djangogramm"),
     mode: 'development',
     entry: {
+        delete_post: "./static/src/delete_post.js",
         like_button: "./static/src/like_button.js",
         subscribe_button: "./static/src/subscribe_button.js"
     },
@@ -19,13 +24,51 @@ module.exports = {
                 template: "./templates/base.html"
             }
         ),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: "[name].[contenthash].css"
+            }
+        )
     ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(
+                {
+                    test: /\.foo\.css$/i,
+                    parallel: true
+                }
+            ),
+            new TerserWebpackPlugin()
+        ]
+    },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
